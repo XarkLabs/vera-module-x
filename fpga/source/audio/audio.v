b42ad5704010f53518e6312d543b7f8f2de65836
Xark <Xark@XarkLabs.com>
2022-10-30 16:00:55 -0700

    NA's volume PR merged

--- fpga/source/audio/audio.v
+++ fpga/source/audio/audio.v
@@ -29,11 +29,11 @@ module audio(
     output wire        i2s_data);
 
     wire        next_sample;
-    wire [15:0] psg_left;
-    wire [15:0] psg_right;
+    wire signed [22:0] psg_left;
+    wire signed [22:0] psg_right;
 
-    wire [15:0] pcm_left;
-    wire [15:0] pcm_right;
+    wire signed [22:0] pcm_left;
+    wire signed [22:0] pcm_right;
 
     //////////////////////////////////////////////////////////////////////////
     // Programmable Sound Generator
@@ -83,21 +83,16 @@ module audio(
     //////////////////////////////////////////////////////////////////////////
     // I2S DAC interface
     //////////////////////////////////////////////////////////////////////////
-    wire [16:0] psg_l = {psg_left[15], psg_left};
-    wire [16:0] psg_r = {psg_right[15], psg_right};
-    wire [16:0] pcm_l = {pcm_left[15], pcm_left};
-    wire [16:0] pcm_r = {pcm_right[15], pcm_right};
 
-    wire [16:0] mix_l = psg_l + pcm_l;
-    wire [16:0] mix_r = psg_r + pcm_r;
-
-    wire [23:0] left_data = {mix_l, 7'b0};
-    wire [23:0] right_data = {mix_r, 7'b0};
-
+    wire [23:0] left_data = psg_left + pcm_left;
+    wire [23:0] right_data = psg_right + pcm_right;
 
 `ifndef SYNTHESIS
-    wire [16:0] u_mix_l = mix_l ^ 17'h10000;
-    wire [16:0] u_mix_r = mix_r ^ 17'h10000;
+    wire [22:0] u_psg_left = psg_left ^ 23'h400000;
+    wire [22:0] u_psg_right = psg_right ^ 23'h400000;
+
+    wire [22:0] u_pcm_left = pcm_left ^ 23'h400000;
+    wire [22:0] u_pcm_right = pcm_right ^ 23'h400000;
 
     wire [23:0] u_left_data = left_data ^ 24'h800000;
     wire [23:0] u_right_data = right_data ^ 24'h800000;
